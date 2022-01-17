951cc845de3a0ca403fdb8421b6c7f83f4b2bdfb
Joey Graham <82058059+JKGRAHAMs@users.noreply.github.com>
2022-01-16 19:36:30 -0600

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Joey Graham on 12/20/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
